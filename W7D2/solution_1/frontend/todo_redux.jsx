a52136a69414410876a3d491551ca9514869fb61
Theodore Summer <TWSummer@gmail.com>
2018-01-22 20:21:17 -0800

    Add middleware to Todo project

---
+++ W7D2/solution_1/frontend/todo_redux.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+// const addLoggingToDispatch = store => next => action => {
+//   console.log(store.getState());
+//   console.log(action);
+//   let result = next(action);
+//   console.log(store.getState());
+//   return result;
+// };
+//
+// function applyMiddlewares(store, ...middlewares) {
+//   let dispatch = store.dispatch;
+//   middlewares.forEach( (middleware) => {
+//     dispatch = middleware(store)(dispatch);
+//   });
+//   Object.assign({}, store, { dispatch });
+// }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  const store = configureStore(preloadedState);
+  // store = applyMiddlewares(store, addLoggingToDispatch);
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
